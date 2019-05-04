e2fc5ba2c4c67ec4795d489d294c7f12dec8506f
PawZawDev <pawzawdev@outlook.com>
2019-05-04 16:02:43 +0200

    Fixed Jerusalem problem, improved user input revised the code a bit new issues arised

--- FindTheMayor.py
+++ FindTheMayor.py
@@ -1,78 +1,104 @@
 """
 Written by Pawel Zawierucha
+Using BeautifulSoup to scrape data from wikipedia
 """
+
+#known issues: missouri city not working (different site layout)
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 
+def desoupify(url):
+    # page=urlopen(url)
+    # soup=BeautifulSoup(page,"html.parser")
+    # whole_table=soup.find("table",attrs={"class":"infobox"})
+    # content=whole_table.text.strip()
+    # return content
+    return (BeautifulSoup(urlopen(url),"html.parser").find("table",attrs={"class":"infobox"})).text.strip()
+
 wiki_url=r"https://en.wikipedia.org/wiki/"
 while(True):
     try:
         city=str(input("Tell me the city: "))
-        city=city.lower()
         if (city=="quit"):
             break
-        elif (city=="jerusalem"):
-            print("well, this city breaks the whole thing")
-            print("Please search for something else")
-            continue
-        final_url=wiki_url+city
+        #delete whitespaces on the ends
+        city=city.strip()
+        #delete excesive spaces
+        city=re.sub(" +"," ",city)
+        #capitalize first letter of every word
+        city=city.title()
+        # #change spaces for the sake of url
+        # city=city.replace(" ","_")
 
-        page=urlopen(final_url)
-        soup=BeautifulSoup(page,"html.parser")
-        whole_table=soup.find("table",attrs={"class":"infobox"})
-        
-        content=whole_table.text.strip()
-    except:
+        final_url=(wiki_url+city).replace(" ","_")
+        try:
+            # page=urlopen(final_url)
+            # soup=BeautifulSoup(page,"html.parser")
+            # whole_table=soup.find("table",attrs={"class":"infobox"})
+            # content=whole_table.text.strip()
+            content=desoupify(final_url)
+        except:
+            #This is for situtions like New York
+            try:
+                # city+="_city"
+                city+=" City"
+                final_url=(wiki_url+city).replace(" ","_")
+                # page=urlopen(final_url)
+                # soup=BeautifulSoup(page,"html.parser")
+                # whole_table=soup.find("table",attrs={"class":"infobox"})
+                # content=whole_table.text.strip()
+                content=desoupify(final_url)
+            except:
+                raise Exception("no results found")
+            else:
+                print("Did you mean {} \"City\"?".format(city[:-5]))
+    except Exception as x:
+        print(x)
         print("Sorry, can't find anything, please try again\n")
         continue
     
-    #mayor_pattern=r"[^A-Za-z]Mayor[A-Za-z ()]*"
-    mayor_pattern=r"[^A-Za-z]Mayor"
+    #there are more words with mayor (like mayoral) but Al can be a name
+    #mayoral can be found in London
+    mayor_pattern=r"[^A-Za-z][Mm]ayor[^(alty)]" 
     dot=r"•"
-    check_mayor=re.search(mayor_pattern,content)
-    hyperlink_pattern=r"\[[1-9]*\]"
+    hyperlink_pattern=r"\[[0-9]*\]"
+    content_table=[]
+    prez=[]
 
-    
-    #the box after mayor (the one we dont want)
-    # print (content)
-    
+    check_mayor=re.search(mayor_pattern,content)
+
     check_area=re.search(r"Area",content)
     if(check_mayor) and (check_area):
-        #print(content)
-        content=content[check_mayor.end():check_area.start()]
+        content=content[check_mayor.end()-1:check_area.start()] #-1 because of regex looking for any character
         check_dot=re.search(dot,content)
-        if (check_dot):
-            content=content[:check_dot.start()]
-        check_hyper=re.match(hyperlink_pattern,content)
-        if(check_hyper):
-            content=content[check_hyper.end():]
-        check_hyper=re.search(hyperlink_pattern,content)
-        if (check_hyper):
-            content=content[:check_hyper.start()]
+        #all this code below to check if there is more than one mayor (aka Jerusalem Exception,
+        # check Jerusalems mayor on wikipedia and see for yourself)
+        while (check_dot)and(check_mayor):
+            content_table.append(content[:check_dot.start()].strip())
+            content=content[check_dot.end():]
+            check_mayor=re.search(mayor_pattern,content)
+            if (check_mayor):
+                content=content[check_mayor.end():]
+            check_dot=re.search(dot,content)
+        
+        if(check_mayor):
+            content_table.append(content.strip())
 
-        # while(1):
-        #     #check_hyper=re.search(hyperlink_pattern,content)
-        #     content=content.replace(hyperlink_pattern,"")
-        #content=content.replace(hyperlink_pattern,"")
-
-
-        # prez=content[check_mayor.start():check_mayor.end()]
-        # check_area=re.search(r"Area",prez)
-        # if(check_area):
-        #     prez=prez[:check_area.start()]
-        # check_mayor_start=re.match(r"[^A-Za-z]?Mayor[^A-Za-z]?",prez)
-        # if(check_mayor_start):
-        #     prez=prez[check_mayor_start.end():]
-
-
-        # print(check_mayor.group())
-        # print(check_mayor.start())
-        # print(check_mayor.end())
-
-        prez=content
-        print("The mayor of "+city+" is "+prez)
+        for ind,con in enumerate(content_table): #need to manipulate the list so require index
+            check_hyper=re.match(hyperlink_pattern,con)
+            if(check_hyper):
+                con=con[check_hyper.end():]
+            check_hyper=re.search(hyperlink_pattern,con)
+            if (check_hyper):
+                con=con[:check_hyper.start()]
+            content_table[ind]=con
+        
+        # city=re.sub("_"," ",city)
+        print("According to \""+final_url+"\":")
+        for c in content_table:
+            print("The mayor of "+city+" is "+c)
         break
     else:
         print("Sorry, can't find anything, please try again\n")
